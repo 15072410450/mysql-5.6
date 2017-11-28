f46a94551b9c827a147c267d42d14e7fd0232bc1
terark <terark@localhost.localdomain>
2017-11-28 19:58:35 +0800

    Update init.sh && start.sh && support-files/mysql.server.sh

--- init.sh
+++ init.sh
@@ -1,27 +1,24 @@
 #/bin/bash
 
-DIR="$(cd "$( dirname "$0")" && pwd)"
+DIR="$(cd "$(dirname "$0")" && pwd)"
 
 if [ "$1" = "prepare" ]; then
     if [ -z "$2" ]; then
-        sed -i "s:/usr/local/mysql-on-terarkdb-4.8-bmi2-0:$PWD:g" support-files/mysql.server
-        sed -i "s:/usr/local/mysql-on-terarkdb-4.8-bmi2-0:$PWD:g" bin/mysqld_safe
-        sed -i "s:/usr/local/mysql-on-terarkdb-4.8-bmi2-0:$PWD:g" my.cnf
-        sed -i "s:/usr/local/mysql-on-terarkdb-4.8-bmi2-0:$PWD:g" start.sh
-        sed -i "s:/usr/local/mysql-on-terarkdb-4.8-bmi2-0:$PWD:g" mysql_on_terarkdb.server
+        sed -i "s:/usr/local/mysql-on-terarkdb-4.8-bmi2-0:$DIR:g" support-files/mysql.server
+        sed -i "s:/usr/local/mysql-on-terarkdb-4.8-bmi2-0:$DIR:g" bin/mysqld_safe
+        sed -i "s:/usr/local/mysql-on-terarkdb-4.8-bmi2-0:$DIR:g" my.cnf
+        sed -i "s:/usr/local/mysql-on-terarkdb-4.8-bmi2-0:$DIR:g" start.sh
         echo "done"
     else
         sed -i "s:/usr/local/mysql-on-terarkdb-4.8-bmi2-0:$2:g" support-files/mysql.server
         sed -i "s:/usr/local/mysql-on-terarkdb-4.8-bmi2-0:$2:g" bin/mysqld_safe
         sed -i "s:/usr/local/mysql-on-terarkdb-4.8-bmi2-0:$2:g" my.cnf
         sed -i "s:/usr/local/mysql-on-terarkdb-4.8-bmi2-0:$2:g" start.sh
-        sed -i "s:/usr/local/mysql-on-terarkdb-4.8-bmi2-0:$2:g" mysql_on_terarkdb.server
         echo "done"
     fi
 elif [ "$1" = "init" ]; then
-    echo "please use this in the basedir"
     echo "start initializing..."
-    scripts/mysql_install_db \
+    $DIR/scripts/mysql_install_db \
 	--defaults-file=$DIR/my.cnf
     echo "done"
 else
--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DIR="$(cd "$( dirname "$0")" && pwd)"
+DIR="$(cd "$(dirname "$0")" && pwd)"
 
-env TerarkZipTable_localTempDir=/usr/local/mysql-on-terarkdb-4.8-bmi2-0/terark-temp \
+env TerarkZipTable_localTempDir=$DIR/terark-temp \
     TerarkZipTable_keyPrefixLen=4 \
     TerarkZipTable_offsetArrayBlockUnits=128 \
     TerarkZipTable_indexCacheRatio=0.001 \
     TerarkZipTable_extendedConfigFile=$DIR/license \
-    support-files/mysql.server start --defaults-file=$DIR/my.cnf
+    $DIR/support-files/mysql.server start --defaults-file=$DIR/my.cnf
