8cb318fb57dfaf50ca7a07aff94f0d3d6d0ac4c2
rockeet <rockeet@gmail.com>
2017-12-28 18:55:28 +0800

    add post-make-install.sh

---
+++ post-make-install.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+CodeDir=$(cd $(dirname $0) && pwd)
+BuildDir=$PWD
+
+for file in `cat $CodeDir/substitue_file_list.txt`
+do
+	sed -i "s:$BuildDir/__MYSQL_INSTALL_DIR__:__MYSQL_INSTALL_DIR__:g" $BuildDir/__MYSQL_INSTALL_DIR__/$file
+done
+
+cp $CodeDir/substitue_file_list.txt __MYSQL_INSTALL_DIR__/my.cnf
+cp $CodeDir/my.cnf-terark-template  __MYSQL_INSTALL_DIR__/my.cnf
+cp $CodeDir/init.sh                 __MYSQL_INSTALL_DIR__
+cp $CodeDir/start*.sh               __MYSQL_INSTALL_DIR__
