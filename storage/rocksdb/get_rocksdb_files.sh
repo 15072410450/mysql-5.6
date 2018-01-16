663b2c74984c45718b88dda91b3f9b90079821a0
rockeet <rockeet@gmail.com>
2018-01-16 11:00:42 +0800

    storage/rocksdb/get_rocksdb_files.sh: delete clock_cache.cc

--- storage/rocksdb/get_rocksdb_files.sh
+++ storage/rocksdb/get_rocksdb_files.sh
@@ -2,9 +2,12 @@
 MKFILE=`mktemp`
 # create and run a simple makefile
 # include rocksdb make file relative to the path of this script
-echo "include rocksdb/src.mk
+echo "#include rocksdb/src.mk ##DO NOT include##
 all:
 	@echo \$(LIB_SOURCES)" > $MKFILE
+
+sed '/clock_cache.cc/d' rocksdb/src.mk >> $MKFILE
+
 for f in `make --makefile $MKFILE`
 do
   echo ../../rocksdb/$f
