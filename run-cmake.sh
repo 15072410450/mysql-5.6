fe9bdaef50c3fc29e9bae05f40281f1d1a2f73d1
frost <vrofze@gmail.com>
2018-12-06 16:41:57 +0800

    Use static zstd lib

--- run-cmake.sh
+++ run-cmake.sh
@@ -4,9 +4,8 @@ if [ -z "$COMPILER" ]; then
 	echo COMPILER is empty 1>&2
 	exit
 fi
-if [ -z "$BMI2" ]; then
-	BMI2=`sh ../../terark/cpu_has_bmi2.sh`
-fi
+DIR="$(cd "$(dirname "$0")" && pwd)"
+BMI2=`sed '/^BMI2=/!d;s/.*=//' $DIR/../make.config`
 ln -sf ../../terark-zip-rocksdb/pkg/terark-zip-rocksdb-Linux-x86_64-${COMPILER}-bmi2-${BMI2} terark-zip-rocksdb-pkg
 ln -sf ../../snappy/package-dir snappy-pkg
 btype=MinSizeRel
@@ -14,9 +13,12 @@ btype=Release
 btype=RelWithDebInfo
 IGNORE_AIO_CHECK=0
 #btype=Debug
-WITH_SSL=/home/opt/gcc-4.8.2.bpkg-r2/gcc-4.8.2.bpkg-r2/
-WITH_SSL=system
+#WITH_SSL=/home/opt/gcc-4.8.2.bpkg-r2/gcc-4.8.2.bpkg-r2/
+#WITH_SSL=system
+WITH_SSL=/disk1/openssl-1.0.1e
 WITH_UNIT_TEST=0
+#CXX_FLAGS="-march=native -DNDEBUG=1 -I../../boost-pkg"
+#CXX_FLAGS="-DNDEBUG=1 -I../../boost-pkg"
 cmake .. \
  -DCMAKE_VERBOSE_MAKEFILE=ON \
  -DCMAKE_SKIP_BUILD_RPATH=ON \
@@ -27,12 +29,13 @@ cmake .. \
  -DWITH_SSL=$WITH_SSL \
  -DWITH_ZLIB=bundled \
  -DWITH_SNAPPY=${PWD}/snappy-pkg \
+ -DWITH_ZSTD=/usr/include \
  -DWITH_TERARKDB=${PWD}/terark-zip-rocksdb-pkg \
  -DMYSQL_MAINTAINER_MODE=0 \
  -DENABLED_LOCAL_INFILE=1 \
  -DWITH_UNIT_TEST=${WITH_UNIT_TEST} \
  -DENABLE_DTRACE=0 \
- -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-march=native -DNDEBUG=1 -I../../boost-pkg" \
+ -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-DNDEBUG=1 -I../../boost-pkg" \
  -DCMAKE_INSTALL_PREFIX=__MYSQL_INSTALL_DIR__
 
 if [ "$IGNORE_AIO_CHECK" -eq 1 ]; then
